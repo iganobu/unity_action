77ad67be2289f4bca206cf9232bd9f49216b774e
igarashi nobuyuki <nobujojo.0501@hotmail.co.jp>
2014-11-04 20:29:26 +0900

    unity-action standard assets

---
+++ Assets/AnimationControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationControl : MonoBehaviour 
+{
+	public Animator animator;
+
+	NavMeshAgent agent;
+	float send_speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		send_speed = agent.velocity.magnitude;
+		animator.SetFloat ("speed", send_speed);
+		//Debug.Log (send_speed);
+	}
+}
---
+++ Assets/MoveToMousePoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToMousePoint : MonoBehaviour
+{
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Ray mouse_ray;
+		RaycastHit hit;
+		if (Input.GetMouseButtonDown(0))
+		{
+			mouse_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(mouse_ray, out hit))
+			{
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
